dca26a7a3ee17df43229e943c3a4071051dc13a0
Mika takala <mika.takala@granitedevices.com>
2017-02-02 17:11:41 +0200

    Added Debug folder stuff without .o/.d/.hex/.elf/.map so that one maybe can compile this using eclipse on another machine.

---
+++ Debug/src/subdir.mk
@@ -0,0 +1,102 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/stm32f4xx_hal_msp.c \
+../src/stm32f4xx_it.c \
+../src/syscalls.c \
+../src/system_stm32f4xx.c \
+../src/usb_device.c \
+../src/usbd_conf.c \
+../src/usbd_core.c \
+../src/usbd_ctlreq.c \
+../src/usbd_custom_hid_if.c \
+../src/usbd_customhid.c \
+../src/usbd_desc.c \
+../src/usbd_ioreq.c 
+
+CPP_SRCS += \
+../src/Command.cpp \
+../src/USBGameController.cpp \
+../src/cDeviceConfig.cpp \
+../src/cFFBDevice.cpp \
+../src/cHardwareConfig.cpp \
+../src/cProfileConfig.cpp \
+../src/ffb.cpp \
+../src/ffb_pro.cpp \
+../src/main.cpp 
+
+OBJS += \
+./src/Command.o \
+./src/USBGameController.o \
+./src/cDeviceConfig.o \
+./src/cFFBDevice.o \
+./src/cHardwareConfig.o \
+./src/cProfileConfig.o \
+./src/ffb.o \
+./src/ffb_pro.o \
+./src/main.o \
+./src/stm32f4xx_hal_msp.o \
+./src/stm32f4xx_it.o \
+./src/syscalls.o \
+./src/system_stm32f4xx.o \
+./src/usb_device.o \
+./src/usbd_conf.o \
+./src/usbd_core.o \
+./src/usbd_ctlreq.o \
+./src/usbd_custom_hid_if.o \
+./src/usbd_customhid.o \
+./src/usbd_desc.o \
+./src/usbd_ioreq.o 
+
+C_DEPS += \
+./src/stm32f4xx_hal_msp.d \
+./src/stm32f4xx_it.d \
+./src/syscalls.d \
+./src/system_stm32f4xx.d \
+./src/usb_device.d \
+./src/usbd_conf.d \
+./src/usbd_core.d \
+./src/usbd_ctlreq.d \
+./src/usbd_custom_hid_if.d \
+./src/usbd_customhid.d \
+./src/usbd_desc.d \
+./src/usbd_ioreq.d 
+
+CPP_DEPS += \
+./src/Command.d \
+./src/USBGameController.d \
+./src/cDeviceConfig.d \
+./src/cFFBDevice.d \
+./src/cHardwareConfig.d \
+./src/cProfileConfig.d \
+./src/ffb.d \
+./src/ffb_pro.d \
+./src/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C++ Compiler'
+	arm-none-eabi-g++ -mcpu=cortex-m4 -mthumb -mfloat-abi=soft -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -fno-move-loop-invariants -Wall -Wextra  -g3 -DDEBUG -DUSE_FULL_ASSERT -DTRACE -DOS_USE_TRACE_SEMIHOSTING_DEBUG -DSTM32F407xx -DUSE_HAL_DRIVER -DHSE_VALUE=8000000 -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4-hal" -std=gnu++11 -fabi-version=0 -fno-exceptions -fno-rtti -fno-use-cxa-atexit -fno-threadsafe-statics -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/stm32f4xx_hal_msp.o: ../src/stm32f4xx_hal_msp.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=soft -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -fno-move-loop-invariants -Wall -Wextra  -g3 -DDEBUG -DUSE_FULL_ASSERT -DTRACE -DOS_USE_TRACE_SEMIHOSTING_DEBUG -DSTM32F407xx -DUSE_HAL_DRIVER -DHSE_VALUE=8000000 -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4-hal" -std=gnu11 -Wno-missing-prototypes -Wno-missing-declarations -MMD -MP -MF"$(@:%.o=%.d)" -MT"src/stm32f4xx_hal_msp.d" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=soft -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -fno-move-loop-invariants -Wall -Wextra  -g3 -DDEBUG -DUSE_FULL_ASSERT -DTRACE -DOS_USE_TRACE_SEMIHOSTING_DEBUG -DSTM32F407xx -DUSE_HAL_DRIVER -DHSE_VALUE=8000000 -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4-hal" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
